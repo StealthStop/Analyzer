c73a48d9541fd617434b7995605d4f3078bbbb2e
BryanCrossman <cros0400@umn.edu>
2020-07-23 17:30:23 -0500

    Add seed method as parameter in module call

--- Analyzer/include/Config.h
+++ Analyzer/include/Config.h
@@ -61,10 +61,10 @@ private:
             else if(module=="MegaJetCombine")               tr.emplaceModule<MegaJetCombine>();
             else if(module=="FatJetCombine")                tr.emplaceModule<FatJetCombine>();
             else if(module=="TrainingNTupleVars")          tr.emplaceModule<TrainingNTupleVars>();
-            else if(module=="MakeStopHemispheres_All")      tr.emplaceModule<MakeStopHemispheres>("Jets",     "AllJets",       "NJets",          "_All");
-            else if(module=="MakeStopHemispheres_1l")       tr.emplaceModule<MakeStopHemispheres>("Jets",     "GoodJets_pt30", "NGoodJets_pt30", "_1l");
-            else if(module=="MakeStopHemispheres_0l")       tr.emplaceModule<MakeStopHemispheres>("Jets",     "GoodJets_pt20", "NGoodJets_pt20", "_0l");
-            else if(module=="MakeStopHemispheres_TaggedTop") tr.emplaceModule<MakeStopHemispheres>("StopJets", "GoodStopJets",  "NGoodStopJets",  "_TaggedTop");
+            else if(module=="MakeStopHemispheres_All")      tr.emplaceModule<MakeStopHemispheres>("Jets",     "AllJets",       "NJets",          "_All", "InvMass");
+            else if(module=="MakeStopHemispheres_1l")       tr.emplaceModule<MakeStopHemispheres>("Jets",     "GoodJets_pt30", "NGoodJets_pt30", "_1l", "InvMass");
+            else if(module=="MakeStopHemispheres_0l")       tr.emplaceModule<MakeStopHemispheres>("Jets",     "GoodJets_pt20", "NGoodJets_pt20", "_0l", "InvMass");
+            else if(module=="MakeStopHemispheres_TaggedTop") tr.emplaceModule<MakeStopHemispheres>("StopJets", "GoodStopJets",  "NGoodStopJets",  "_TaggedTop", "TopSeed");
             else if(module=="StopJets")                     tr.emplaceModule<StopJets>();
             else if(module=="DeepEventShape")               tr.emplaceModule<DeepEventShape>(DeepESMCfg, ModelFile);
             else if(module=="DeepEventShape_NonIsoMuon")    tr.emplaceModule<DeepEventShape>(DeepESMCfg_NonIsoMuon, ModelFile);
