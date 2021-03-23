e8706c5c175b92b63d84ed4e4e0998897ade4ad9
Semra <Semra.Turkcapar@cern.ch>
2021-03-23 06:53:03 -0500

    updated for RefAN triggers with their preselections_pt45 and our triggers with their preselections

--- Analyzer/src/HadTriggers_Analyzer.cc
+++ Analyzer/src/HadTriggers_Analyzer.cc
@@ -93,8 +93,56 @@ void HadTriggers_Analyzer::InitHistos()
             }
         }
     }
+   
+    // --------------------------------------------------
+    // their triggers + their preselections with our pt45
+    // --------------------------------------------------
+    std::vector<std::string> theirTrigTags { "TheirHadIsoMu" };
 
-}
+    for( std::string effTag : effTags )
+    {
+        for( std::string theirTrigTag : theirTrigTags )
+        {
+            for( std::string trigTag : trigTags )
+            {
+                for( std::string nBJetCutTag : nBJetCutTags )
+                {
+                    my_histos.emplace( "h_"+effTag+"_"+theirTrigTag+"_"+trigTag+"_"+nBJetCutTag+"_HT", std::make_shared<TH1D>(("h_"+effTag+"_"+theirTrigTag+"_"+trigTag+"_"+nBJetCutTag+"_HT").c_str(), ("h_"+effTag+"_"+theirTrigTag+"_"+trigTag+"_"+nBJetCutTag+"_HT").c_str(), htbins, htbinEdges ) );
+                    my_histos.emplace( "h_"+effTag+"_"+theirTrigTag+"_"+trigTag+"_"+nBJetCutTag+"_6thJetPt", std::make_shared<TH1D>(("h_"+effTag+"_"+theirTrigTag+"_"+trigTag+"_"+nBJetCutTag+"_6thJetPt").c_str(), ("h_"+effTag+"_"+theirTrigTag+"_"+trigTag+"_"+nBJetCutTag+"_6thJetPt").c_str(), ptbins, ptbinEdges ) );
+                    my_histos.emplace( "h_"+effTag+"_"+theirTrigTag+"_"+trigTag+"_"+nBJetCutTag+"_NBJet", std::make_shared<TH1D>(("h_"+effTag+"_"+theirTrigTag+"_"+trigTag+"_"+nBJetCutTag+"_NBJet").c_str(), ("h_"+effTag+"_"+theirTrigTag+"_"+trigTag+"_"+nBJetCutTag+"_NBJet").c_str(), bjetbins, bjetbinEdges ) );
+
+                    my_2d_histos.emplace( "h_"+effTag+"_"+theirTrigTag+"_"+trigTag+"_"+nBJetCutTag+"_HTvs6thJetPt", std::make_shared<TH2D>( ( "h_"+effTag+"_"+theirTrigTag+"_"+trigTag+"_"+nBJetCutTag+"_HTvs6thJetPt" ).c_str(), ( "h_"+effTag+"_"+theirTrigTag+"_"+trigTag+"_"+nBJetCutTag+"_HTvs6thJetPt" ).c_str(), htbins, htbinEdges, ptbins, ptbinEdges ) );
+
+                }
+            }
+        }
+    }
+ 
+    // -----------------------------------------------
+    // our triggers + reference analysis preselections
+    // -----------------------------------------------
+    std::vector<std::string> ourTrigTags { "OurHadIsoMu" };
+    
+    for( std::string effTag : effTags )
+    {
+        for( std::string ourTrigTag : ourTrigTags ) 
+        {
+            for( std::string trigTag : trigTags )
+            {
+                for( std::string nBJetCutTag : nBJetCutTags )
+                {
+                    my_histos.emplace( "h_"+effTag+"_"+ourTrigTag+"_"+trigTag+"_"+nBJetCutTag+"_HT", std::make_shared<TH1D>(("h_"+effTag+"_"+ourTrigTag+"_"+trigTag+"_"+nBJetCutTag+"_HT").c_str(), ("h_"+effTag+"_"+ourTrigTag+"_"+trigTag+"_"+nBJetCutTag+"_HT").c_str(), htbins, htbinEdges ) );
+                    my_histos.emplace( "h_"+effTag+"_"+ourTrigTag+"_"+trigTag+"_"+nBJetCutTag+"_6thJetPt", std::make_shared<TH1D>(("h_"+effTag+"_"+ourTrigTag+"_"+trigTag+"_"+nBJetCutTag+"_6thJetPt").c_str(), ("h_"+effTag+"_"+ourTrigTag+"_"+trigTag+"_"+nBJetCutTag+"_6thJetPt").c_str(), ptbins, ptbinEdges ) );
+                    my_histos.emplace( "h_"+effTag+"_"+ourTrigTag+"_"+trigTag+"_"+nBJetCutTag+"_NBJet", std::make_shared<TH1D>(("h_"+effTag+"_"+ourTrigTag+"_"+trigTag+"_"+nBJetCutTag+"_NBJet").c_str(), ("h_"+effTag+"_"+ourTrigTag+"_"+trigTag+"_"+nBJetCutTag+"_NBJet").c_str(), bjetbins, bjetbinEdges ) );
+                    
+                    my_2d_histos.emplace( "h_"+effTag+"_"+ourTrigTag+"_"+trigTag+"_"+nBJetCutTag+"_HTvs6thJetPt", std::make_shared<TH2D>( ( "h_"+effTag+"_"+ourTrigTag+"_"+trigTag+"_"+nBJetCutTag+"_HTvs6thJetPt" ).c_str(), ( "h_"+effTag+"_"+ourTrigTag+"_"+trigTag+"_"+nBJetCutTag+"_HTvs6thJetPt" ).c_str(), htbins, htbinEdges, ptbins, ptbinEdges ) );
+ 
+                }          
+            }
+        }
+    }
+
+} //
 
 
 void HadTriggers_Analyzer::Loop(NTupleReader& tr, double, int maxevents, bool)
@@ -104,24 +152,28 @@ void HadTriggers_Analyzer::Loop(NTupleReader& tr, double, int maxevents, bool)
         const auto& eventCounter        = tr.getVar<int>("eventCounter");
         my_histos["EventCounter"]->Fill( eventCounter );
 
-        const auto& runtype                  = tr.getVar<std::string>("runtype");
-        const auto& filetag                  = tr.getVar<std::string>("filetag");
+        const auto& runtype                   = tr.getVar<std::string>("runtype");
+        const auto& filetag                   = tr.getVar<std::string>("filetag");
         // for our analysis
-        const auto& NGoodJets_pt45           = tr.getVar<int>("NGoodJets_pt45");
-        const auto& NGoodBJets_pt45          = tr.getVar<int>("NGoodBJets_pt45");
-        const auto& HT_trigger_pt45          = tr.getVar<double>("HT_trigger_pt45");
-        const auto& passBaseline0l_hadTrig   = tr.getVar<bool>("passBaseline0l_hadTrig");
-        const auto& passBaseline0l_hadMuTrig = tr.getVar<bool>("passBaseline0l_hadMuTrig");
-        const auto& passTriggerAllHad        = tr.getVar<bool>("passTriggerAllHad");
-        // for reference analysis AN-2016/411
-        const auto& Jets                     = tr.getVec<TLorentzVector>("Jets");
-        const auto& GoodJets_pt40            = tr.getVec<bool>("GoodJets_pt40");
-        const auto& NGoodJets_pt40           = tr.getVar<int>("NGoodJets_pt40");
-        const auto& NGoodBJets_pt30          = tr.getVar<int>("NGoodBJets_pt30");
-        const auto& HT_trigger_pt30          = tr.getVar<double>("HT_trigger_pt30");
-        const auto& passBaseline0l_refAN     = tr.getVar<bool>("passBaseline0l_refAN");
-        const auto& passTriggerMuonsRefAN    = tr.getVar<bool>("passTriggerMuonsRefAN");
-        const auto& passTriggerRefAN         = tr.getVar<bool>("passTriggerRefAN");
+        const auto& NGoodJets_pt45            = tr.getVar<int>("NGoodJets_pt45");
+        const auto& NGoodBJets_pt45           = tr.getVar<int>("NGoodBJets_pt45");
+        const auto& HT_trigger_pt45           = tr.getVar<double>("HT_trigger_pt45");
+        const auto& passBaseline0l_hadTrig    = tr.getVar<bool>("passBaseline0l_hadTrig");
+        const auto& passBaseline0l_hadMuTrig  = tr.getVar<bool>("passBaseline0l_hadMuTrig");
+        const auto& passTriggerAllHad         = tr.getVar<bool>("passTriggerAllHad");
+        // for reference analysis AN-2016/411 
+        const auto& Jets                      = tr.getVec<TLorentzVector>("Jets");
+        const auto& GoodJets_pt40             = tr.getVec<bool>("GoodJets_pt40");
+        const auto& NGoodJets_pt40            = tr.getVar<int>("NGoodJets_pt40");
+        const auto& NGoodBJets_pt30           = tr.getVar<int>("NGoodBJets_pt30");
+        const auto& HT_trigger_pt30           = tr.getVar<double>("HT_trigger_pt30");
+        const auto& passBaseline0l_refAN      = tr.getVar<bool>("passBaseline0l_refAN");
+        const auto& passTriggerMuonsRefAN     = tr.getVar<bool>("passTriggerMuonsRefAN");
+        const auto& passTriggerRefAN          = tr.getVar<bool>("passTriggerRefAN");
+        // their triggers + their preselections with our pt45
+        const auto& GoodJets_pt45             = tr.getVec<bool>("GoodJets_pt45");
+        const auto& passBaseline0l_refAN_pt45 = tr.getVar<bool>("passBaseline0l_refAN_pt45");
+
         bool pass_2bjetCut   = NGoodBJets_pt30 == 2; 
         bool pass_3bjetCut   = NGoodBJets_pt30 == 3;
         bool pass_ge4bjetCut = NGoodBJets_pt30 >= 4;
@@ -140,6 +192,20 @@ void HadTriggers_Analyzer::Loop(NTupleReader& tr, double, int maxevents, bool)
             if (njets == 6)
             {
                 SixthJetPt = Jets.at(j).Pt();
+                break;
+            }
+        }
+
+        int njetspt45 = 0;
+        double SixthJetPt45 = 0.0;
+        for (unsigned int j = 0; j < Jets.size(); j++)
+        {
+            if (!GoodJets_pt45[j]) continue;
+            njetspt45++;
+
+            if (njetspt45 == 6)
+            {
+                SixthJetPt45 = Jets.at(j).Pt();
                 break;
             }
         }
@@ -184,16 +250,19 @@ void HadTriggers_Analyzer::Loop(NTupleReader& tr, double, int maxevents, bool)
         
         if( (filetag.find("Data_JetHT") != std::string::npos || runtype == "MC") ) 
         {
+            // -------------------------
             // for our analysis triggers
+            // -------------------------
             const std::map<std::string, bool> cut_map_hadTriggers 
             {
                 { "had_trig_pt45",   passBaseline0l_hadTrig && passTriggerAllHad },                                               
                 { "had_noTrig_pt45", passBaseline0l_hadTrig }, 
             };
-        
             fillHistos(cut_map_hadTriggers, passTriggerAllHad, HT_trigger_pt45, NGoodJets_pt45, NGoodBJets_pt45, weight);
 
+            // -------------------------------
             // for reference analysis triggers
+            // -------------------------------
             const std::map<std::string, bool> cut_map_refAnTriggers
             {  
                 { "refAnHad_trig_ge2bjetCut", passBaseline0l_refAN && passTriggerRefAN                    }, 
@@ -206,23 +275,28 @@ void HadTriggers_Analyzer::Loop(NTupleReader& tr, double, int maxevents, bool)
                 { "refAnHad_noTrig_3bjetCut",   passBaseline0l_refAN && pass_3bjetCut   },
                 { "refAnHad_noTrig_ge4bjetCut", passBaseline0l_refAN && pass_ge4bjetCut },
             };
-           
             fillHistosRefAN(cut_map_refAnTriggers, passTriggerRefAN, HT_trigger_pt30, SixthJetPt, NGoodBJets_pt30, weight);
 
         }
-
+        
+        // ---------------------------------------------------------
+        // -- Trigger Efficiency on the SingleMuon Dataset and MC
+        // ---------------------------------------------------------
         if ( (filetag.find("Data_SingleMuon") != std::string::npos || runtype == "MC") )
         {
+            // -------------------------
             // for our analysis triggers
+            // -------------------------
             const std::map<std::string, bool> cut_map_hadMuTriggers
             {   
                 { "had_IsoMu_trig_pt45",   passBaseline0l_hadMuTrig && passTriggerAllHad},
                 { "had_IsoMu_noTrig_pt45", passBaseline0l_hadMuTrig },  
             };
-
             fillHistos(cut_map_hadMuTriggers, passTriggerAllHad, HT_trigger_pt45, NGoodJets_pt45, NGoodBJets_pt45, weight);
 
+            // -------------------------------
             // for reference analysis triggers
+            // -------------------------------
             const std::map<std::string, bool> cut_map_refAnMuTriggers
             {
                 { "refAnHadIsoMu_trig_ge2bjetCut", passBaseline0l_refAN && passTriggerMuonsRefAN && passTriggerRefAN                    },
@@ -234,10 +308,43 @@ void HadTriggers_Analyzer::Loop(NTupleReader& tr, double, int maxevents, bool)
                 { "refAnHadIsoMu_noTrig_2bjetCut",   passBaseline0l_refAN && passTriggerMuonsRefAN && pass_2bjetCut   },
                 { "refAnHadIsoMu_noTrig_3bjetCut",   passBaseline0l_refAN && passTriggerMuonsRefAN && pass_3bjetCut   },
                 { "refAnHadIsoMu_noTrig_ge4bjetCut", passBaseline0l_refAN && passTriggerMuonsRefAN && pass_ge4bjetCut },
-
             };
-
             fillHistosRefAN(cut_map_refAnMuTriggers, passTriggerRefAN, HT_trigger_pt30, SixthJetPt, NGoodBJets_pt30, weight);
+            
+            // --------------------------------------------------
+            // their triggers + their preselections with our pt45
+            // --------------------------------------------------
+            const std::map<std::string, bool> cut_map_theirTriggers
+            {   
+                { "TheirHadIsoMu_trig_ge2bjetCut",   passBaseline0l_refAN_pt45 && passTriggerMuonsRefAN && passTriggerRefAN                    },
+                { "TheirHadIsoMu_trig_2bjetCut",     passBaseline0l_refAN_pt45 && passTriggerMuonsRefAN && passTriggerRefAN && pass_2bjetCut   },
+                { "TheirHadIsoMu_trig_3bjetCut",     passBaseline0l_refAN_pt45 && passTriggerMuonsRefAN && passTriggerRefAN && pass_3bjetCut   },
+                { "TheirHadIsoMu_trig_ge4bjetCut",   passBaseline0l_refAN_pt45 && passTriggerMuonsRefAN && passTriggerRefAN && pass_ge4bjetCut },
+                
+                { "TheirHadIsoMu_noTrig_ge2bjetCut", passBaseline0l_refAN_pt45 && passTriggerMuonsRefAN                    },
+                { "TheirHadIsoMu_noTrig_2bjetCut",   passBaseline0l_refAN_pt45 && passTriggerMuonsRefAN && pass_2bjetCut   },
+                { "TheirHadIsoMu_noTrig_3bjetCut",   passBaseline0l_refAN_pt45 && passTriggerMuonsRefAN && pass_3bjetCut   },
+                { "TheirHadIsoMu_noTrig_ge4bjetCut", passBaseline0l_refAN_pt45 && passTriggerMuonsRefAN && pass_ge4bjetCut },
+            };
+            fillHistosRefAN(cut_map_theirTriggers, passTriggerRefAN, HT_trigger_pt45, SixthJetPt45, NGoodBJets_pt45, weight);
+            
+            // -----------------------------------------------
+            // our triggers + reference analysis preselections
+            // -----------------------------------------------
+            const std::map<std::string, bool> cut_map_ourTriggers
+            {
+                { "OurHadIsoMu_trig_ge2bjetCut",   passBaseline0l_refAN && passTriggerMuonsRefAN && passTriggerAllHad                    },
+                { "OurHadIsoMu_trig_2bjetCut",     passBaseline0l_refAN && passTriggerMuonsRefAN && passTriggerAllHad && pass_2bjetCut   },
+                { "OurHadIsoMu_trig_3bjetCut",     passBaseline0l_refAN && passTriggerMuonsRefAN && passTriggerAllHad && pass_3bjetCut   },
+                { "OurHadIsoMu_trig_ge4bjetCut",   passBaseline0l_refAN && passTriggerMuonsRefAN && passTriggerAllHad && pass_ge4bjetCut },
+
+                { "OurHadIsoMu_noTrig_ge2bjetCut", passBaseline0l_refAN && passTriggerMuonsRefAN                    },
+                { "OurHadIsoMu_noTrig_2bjetCut",   passBaseline0l_refAN && passTriggerMuonsRefAN && pass_2bjetCut   },
+                { "OurHadIsoMu_noTrig_3bjetCut",   passBaseline0l_refAN && passTriggerMuonsRefAN && pass_3bjetCut   },
+                { "OurHadIsoMu_noTrig_ge4bjetCut", passBaseline0l_refAN && passTriggerMuonsRefAN && pass_ge4bjetCut },
+            };
+            fillHistosRefAN(cut_map_ourTriggers, passTriggerAllHad, HT_trigger_pt30, SixthJetPt, NGoodBJets_pt30, weight);
+
         }
     }
 }
@@ -308,7 +415,7 @@ void HadTriggers_Analyzer::fillHistos( const std::map<std::string, bool>& cutMap
 
 // function for the reference analysis histos
 void HadTriggers_Analyzer::fillHistosRefAN( const std::map<std::string, bool>& cutMap, bool passTriggerRefAN, double HT, double pt, int nbjet, double weight )
-{
+{ 
     for( auto& kv : cutMap )
     {
         if( kv.second )
@@ -325,6 +432,7 @@ void HadTriggers_Analyzer::fillHistosRefAN( const std::map<std::string, bool>& c
                 my_histos["h_numerator_"+kv.first+"_NBJet"]->Fill( nbjet, weight );
                 my_2d_histos["h_numerator_"+kv.first+"_HTvs6thJetPt"]->Fill( HT, pt, weight );
             }
+
         }
     }
 }
