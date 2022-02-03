42d338eaaec5860e16656802c096729ab522e2ae
Josh Hiltbrand <hiltbran@umn.edu>
2022-02-03 06:54:51 -0600

    keep some tweaks to python plotters

--- Analyzer/test/stackPlotter_aux_ttVsSig_massReg.py
+++ Analyzer/test/stackPlotter_aux_ttVsSig_massReg.py
@@ -7,12 +7,15 @@ selections = ["_0l",
 ]
 
 histograms = {
-    "h_DoubleDisCo_massReg?_Njets@_ABCD"  : {"logY" : True,  "orders" : list(xrange(7,12)), "Y" : {"title" : "A.U.", "min" : 2e-3}, "X" : {"title" : "Regression Mass [GeV]",   "rebin" : 2, "min" : 0,  "max" : 1500}},
-    "h_DoubleDisCo_massReg?_Njets11incl_ABCD"  : {"logY" : True, "Y" : {"title" : "A.U.", "min" : 2e-3}, "X" : {"title" : "Regression Mass [GeV]",   "rebin" : 2, "min" : 0,  "max" : 1500}},
+    "h_DoubleDisCo_massReg?_Njets@_ABCD"  : {"logY" : False,  "orders" : list(xrange(7,12)), "Y" : {"title" : "A.U.", "min" : 2e-3}, "X" : {"title" : "Regression Mass [GeV]",   "rebin" : 2, "min" : 0,  "max" : 1500}},
+    "h_DoubleDisCo_massReg?_Njets11incl_ABCD"  : {"logY" : False, "Y" : {"title" : "A.U.", "min" : 2e-3}, "X" : {"title" : "Regression Mass [GeV]",   "rebin" : 2, "min" : 0,  "max" : 1500}},
+    "h_DoubleDisCo_massReg?_Njets12incl_ABCD"  : {"logY" : False, "Y" : {"title" : "A.U.", "min" : 2e-3}, "X" : {"title" : "Regression Mass [GeV]",   "rebin" : 2, "min" : 0,  "max" : 1500}},
     "h_DoubleDisCo_disc1?_Njets@"  : {"logY" : False,  "orders" : list(xrange(7,12)), "Y" : {"title" : "A.U.", "min" : 2e-3}, "X" : {"title" : "Neural Network Disc. 1",   "rebin" : 2, "min" : 0,  "max" : 1}},
     "h_DoubleDisCo_disc2?_Njets@"  : {"logY" : False,  "orders" : list(xrange(7,12)), "Y" : {"title" : "A.U.", "min" : 2e-3}, "X" : {"title" : "Neural Network Disc. 2",   "rebin" : 2, "min" : 0,  "max" : 1}},
     "h_DoubleDisCo_disc1?_Njets11incl"  : {"logY" : False,  "Y" : {"title" : "A.U.", "min" : 2e-3}, "X" : {"title" : "Neural Network Disc. 1",   "rebin" : 2, "min" : 0,  "max" : 1}},
     "h_DoubleDisCo_disc2?_Njets11incl"  : {"logY" : False,  "Y" : {"title" : "A.U.", "min" : 2e-3}, "X" : {"title" : "Neural Network Disc. 2",   "rebin" : 2, "min" : 0,  "max" : 1}},
+    "h_DoubleDisCo_disc1?_Njets12incl"  : {"logY" : False,  "Y" : {"title" : "A.U.", "min" : 2e-3}, "X" : {"title" : "Neural Network Disc. 1",   "rebin" : 2, "min" : 0,  "max" : 1}},
+    "h_DoubleDisCo_disc2?_Njets12incl"  : {"logY" : False,  "Y" : {"title" : "A.U.", "min" : 2e-3}, "X" : {"title" : "Neural Network Disc. 2",   "rebin" : 2, "min" : 0,  "max" : 1}},
 
 }
 
