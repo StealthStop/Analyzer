c2313edd38b22bce089e2f3f6c3be065a3c47d01
Josh Hiltbrand <hiltbran@umn.edu>
2022-03-23 17:09:45 -0500

    add some comments in code to help user

--- Analyzer/test/condor/runSplitSignal.sh
+++ Analyzer/test/condor/runSplitSignal.sh
@@ -1,15 +1,17 @@
 #! /bin/bash
 
-INPUTFILE=$1
-OUTPUTDIR=$2
-TREENAME=$3
+INPUTFILE=$1 # full path to a ROOT file on EOS and properly uses the xrd address
+OUTPUTDIR=$2 # full path to user's EOS area where output ROOT files will be copied
+TREENAME=$3  # name of the TTree inside the input ROOT file to read
 
+# Get a ROOT without using CMSSW
 source /cvmfs/sft.cern.ch/lcg/views/LCG_99/x86_64-centos7-gcc10-opt/setup.sh
 
-python runSplitSignal.py --inputFile $INPUTFILE --ttreeName $TREENAME
+python runSplitSignal.py --inputFile $INPUTFILE --ttreePath $TREENAME
 
+# Copy output files to EOS and remove from job node
 for FILE in *.root
 do
-    xrdcp -r $FILE $OUTPUTDIR
+    xrdcp $FILE $OUTPUTDIR
     rm $FILE
 done
--- Analyzer/test/condor/submitSplitSignal.py
+++ Analyzer/test/condor/submitSplitSignal.py
@@ -1,13 +1,24 @@
-import ROOT, subprocess, os, argparse
+import subprocess, os, argparse
 
+# Prepare and submit jobs to condor to run runSplitSignal.py
+# eosPath   : full path to a directory on EOS containing signal ROOT files (redirector handled automatically)
+# outPath   : sub path to be created on the users EOS are to put output ROOT files
+# ttreePath : path to the TTree inside of the input ROOT file
+# wildcard  : some sort of wildcard phrase to select certain ROOT files in the eosPath
+# noSubmit  : do not submit the jobs, but create everything needed
+#
+# Example call:
+# python submitSplitSignal.py --eosPath  /eos/uscms/store/user/semrat/SusyRA2Analysis2015/Run2ProductionV20/
+#                             --outPath  NewSignal
+#                             --wildcard "*300to1400*"
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--eosPath"  , dest="eosPath"  , help="Path to files on EOS"   , type=str,  required=True                     )
-    parser.add_argument("--outPath"  , dest="outPath"  , help="Output path for jobs"   , type=str,  default="SplitJobs"               )
-    parser.add_argument("--treeName" , dest="treeName" , help="TTree name to read"     , type=str,  default="TreeMaker2/PreSelection" )
-    parser.add_argument("--wildcard" , dest="wildcard" , help="Wildcards for picking"  , type=str,  default="*300to1400*"             )
-    parser.add_argument("--noSubmit" , dest="noSubmit" , help="Do not submit to condor",            default=False, action="store_true")
+    parser.add_argument("--eosPath"  , dest="eosPath"  , help="Path to files on EOS"   , type=str, required=True                     )
+    parser.add_argument("--outPath"  , dest="outPath"  , help="Output path for jobs"   , type=str, default="SplitJobs"               )
+    parser.add_argument("--ttreePath", dest="ttreePath", help="TTree name to read"     , type=str, default="TreeMaker2/PreSelection" )
+    parser.add_argument("--wildcard" , dest="wildcard" , help="Wildcards for picking"  , type=str, default="*300to1400*"             )
+    parser.add_argument("--noSubmit" , dest="noSubmit" , help="Do not submit to condor",           default=False, action="store_true")
     args = parser.parse_args()
 
     USER = os.getenv("USER")
@@ -32,22 +43,21 @@ if __name__ == '__main__':
     fout.write("WhenToTransferOutput  = ON_EXIT\n")
     fout.write("x509userproxy         = $ENV(X509_USER_PROXY)\n\n")
 
-    # Use EOS commands to get the list of files at the eosPath that contain the wildcard phrase
+    # Use EOS commands to get the list of files at the eosPath that satisfy the wildcard phrase
     proc     = subprocess.Popen(["eos", "root://cmseos.fnal.gov", "ls", "%s/%s.root"%(args.eosPath, args.wildcard)], stdout=subprocess.PIPE)
     eosls    = proc.stdout.readlines()
     fileList = [line.decode("utf8").strip() for line in eosls]
 
+    # Give one input ROOT file to each job
     iFile = 0
     for file in fileList:
-        fout.write("Arguments = %s/%s %s %s\n"%(args.eosPath.replace("/eos/uscms/", "root://cmseos.fnal.gov///"), file, outputDir, args.treeName))
+        fout.write("Arguments = %s/%s %s %s\n"%(args.eosPath.replace("/eos/uscms/", "root://cmseos.fnal.gov///"), file, outputDir, args.ttreePath))
         fout.write("Output    = %s/log-files/sigSplit_%s.stdout\n"%(args.outPath, iFile))
         fout.write("Error     = %s/log-files/sigSplit_%s.stderr\n"%(args.outPath, iFile))
         fout.write("Log       = %s/log-files/sigSplit_%s.log\n"%(args.outPath, iFile))
         fout.write("Queue\n\n")
-    
     fout.close()
 
     if not args.noSubmit: 
-        os.system('mkdir -p logs')
         os.system("echo 'condor_submit condor_submit.txt'")
         os.system('condor_submit condor_submit.txt')
