e45fbda6933ebc603047f7526378d47a7d22a56c
Josh Hiltbrand <hiltbran@umn.edu>
2022-12-15 08:15:25 -0600

    formalize nnInput aux

--- Analyzer/test/Plotters/General/stackPlotter_aux_nnInputs_dataVsMC.py
+++ Analyzer/test/Plotters/General/stackPlotter_aux_nnInputs_dataVsMC.py
@@ -8,7 +8,7 @@ selections = ["0l_blind_ABCD", "1l_blind_ABCD", "2l_blind_ABCD", "0l_ABCD", "1l_
 comboJets  = ["8",             "7",             "6",             "8",       "7",       "6",       "8",             "7"            ]
 
 histograms = {
-    "h_Jet@_PtrHT_cm_?"                  : {"logY" : True,  "orders" : list(xrange(1,8)), "Y" : {"title" : "Number of Events", "min" : 0.2}, "X" : {"title" : "Jet @ p_{T}/H_{T}",                   "rebin" : 4, "min" :  0,    "max" :    1}},
+    "h_Jet@_PtrHT_cm_?"                  : {"logY" : False, "orders" : list(xrange(1,8)), "Y" : {"title" : "Number of Events", "min" : 0.2}, "X" : {"title" : "Jet @ p_{T}/H_{T}",                   "rebin" : 4, "min" :  0,    "max" :    1}},
     "h_Jet@_Pt_cm_?"                     : {"logY" : True,  "orders" : list(xrange(1,8)), "Y" : {"title" : "Number of Events", "min" : 0.2}, "X" : {"title" : "Jet @ p_{T} [GeV]",                   "rebin" : 8, "min" :  0,    "max" : 1500}},
     "h_Jet@_Eta_cm_?"                    : {"logY" : False, "orders" : list(xrange(1,8)), "Y" : {"title" : "Number of Events", "min" : 0.2}, "X" : {"title" : "Jet @ #eta",                          "rebin" : 1, "min" : -4,    "max" :    4}},
     "h_Jet@_Phi_cm_?"                    : {"logY" : False, "orders" : list(xrange(1,8)), "Y" : {"title" : "Number of Events", "min" : 0.2}, "X" : {"title" : "Jet @ #phi",                          "rebin" : 1, "min" : -6,    "max" :    6}},
@@ -22,13 +22,13 @@ histograms = {
     "h_FWM@_top6_?"                      : {"logY" : False, "orders" : list(xrange(2,6)), "Y" : {"title" : "Number of Events", "min" : 0.2}, "X" : {"title" : "Fox-Wolfram Moment @",                "rebin" : 1, "min" :  0,    "max" :    1}},
     "h_JMT_ev@_top6_?"                   : {"logY" : False, "orders" : list(xrange(0,3)), "Y" : {"title" : "Number of Events", "min" : 0.2}, "X" : {"title" : "Jet p-E Eigenvalue @",                "rebin" : 1, "min" :  0,    "max" :    1}},
    
-    "h_Stop@_PtrHT_cm_OldSeed_?"         : {"logY" : True,  "orders" : list(xrange(1,3)), "Y" : {"title" : "Number of Events", "min" : 0.2}, "X" : {"title" : "Stop @ p_{T}/H_{T}",                  "rebin" : 4, "min" :  0,    "max" :    1}}, 
+    "h_Stop@_PtrHT_cm_OldSeed_?"         : {"logY" : False, "orders" : list(xrange(1,3)), "Y" : {"title" : "Number of Events", "min" : 0.2}, "X" : {"title" : "Stop @ p_{T}/H_{T}",                  "rebin" : 4, "min" :  0,    "max" :    1}}, 
     "h_Stop@_Pt_cm_OldSeed_?"            : {"logY" : True,  "orders" : list(xrange(1,3)), "Y" : {"title" : "Number of Events", "min" : 0.2}, "X" : {"title" : "Stop @ p_{T} [GeV]",                  "rebin" : 8, "min" :  0,    "max" : 1500}},
     "h_Stop@_Mass_cm_OldSeed_?"          : {"logY" : False, "orders" : list(xrange(1,3)), "Y" : {"title" : "Number of Events", "min" : 0.2}, "X" : {"title" : "Stop @ mass [GeV]",                   "rebin" : 2, "min" :  0,    "max" : 1500}},
     "h_Stop@_Eta_cm_OldSeed_?"           : {"logY" : False, "orders" : list(xrange(1,3)), "Y" : {"title" : "Number of Events", "min" : 0.2}, "X" : {"title" : "Stop @ #eta",                         "rebin" : 1, "min" : -4,    "max" :    4}},
     "h_Stop@_Phi_cm_OldSeed_?"           : {"logY" : False, "orders" : list(xrange(1,3)), "Y" : {"title" : "Number of Events", "min" : 0.2}, "X" : {"title" : "Stop @ #phi",                         "rebin" : 1, "min" : -6,    "max" :    6}},
    
-    "h_Stop@_PtrHT_cm_TopSeed_?"         : {"logY" : True,  "orders" : list(xrange(1,3)), "Y" : {"title" : "Number of Events", "min" : 0.2}, "X" : {"title" : "Stop @ p_{T}/H_{T}",                  "rebin" : 4, "min" :  0,    "max" :    1}}, 
+    "h_Stop@_PtrHT_cm_TopSeed_?"         : {"logY" : False, "orders" : list(xrange(1,3)), "Y" : {"title" : "Number of Events", "min" : 0.2}, "X" : {"title" : "Stop @ p_{T}/H_{T}",                  "rebin" : 4, "min" :  0,    "max" :    1}}, 
     "h_Stop@_Pt_cm_TopSeed_?"            : {"logY" : True,  "orders" : list(xrange(1,3)), "Y" : {"title" : "Number of Events", "min" : 0.2}, "X" : {"title" : "Stop @ p_{T} [GeV]",                  "rebin" : 8, "min" :  0,    "max" : 1500}},
     "h_Stop@_Mass_cm_TopSeed_?"          : {"logY" : False, "orders" : list(xrange(1,3)), "Y" : {"title" : "Number of Events", "min" : 0.2}, "X" : {"title" : "Stop @ mass [GeV]",                   "rebin" : 2, "min" :  0,    "max" : 1500}},
     "h_Stop@_Eta_cm_TopSeed_?"           : {"logY" : False, "orders" : list(xrange(1,3)), "Y" : {"title" : "Number of Events", "min" : 0.2}, "X" : {"title" : "Stop @ #eta",                         "rebin" : 1, "min" : -4,    "max" :    4}},
@@ -45,11 +45,9 @@ for selection in selections:
     histograms["h_combined%sthJet_Mass_cm_%s"%(comboJet, selection)]    = {"logY" : False, "Y" : {"title" : "Number of Events", "min" : 0.2}, "X" : {"title" : "Combined %s to Last Jet mass [GeV]"%(comboJet),   "rebin" : 2, "min" :  0,    "max" :  150}},
     histograms["h_combined%sthJet_Energy_cm_%s"%(comboJet, selection)]  = {"logY" : True,  "Y" : {"title" : "Number of Events", "min" : 0.2}, "X" : {"title" : "Combined %s to Last Jet Energy [GeV]"%(comboJet), "rebin" : 8, "min" :  0,    "max" : 1500}},
 
-
 data = {
     "Data" : {"name" : "Data", "color" : ROOT.kBlack, "lstyle" : 1, "mstyle" : 8, "lsize" : 3, "msize" : 1.3}
 }
- 
 
 backgrounds = {
     "TT"       : {"name" : "t#bar{t} + jets", "color" : 40,  "lstyle" : 1, "mstyle" : 8, "lsize" : 3, "msize" : 0},
